d791beb505a9be9c0399941e439157e0376a173f
Martin Vala <Martin.Vala@cern.ch>
2012-05-14 14:15:10 +0200

    Added option to control running parallel from outside run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-
+MY_PARALLEL=1
+if [ -n "$1" ];then
+  MY_PARALLEL=$1
+fi
 MY_DIR="$(dirname $(readlink -m $0))"
 cd $MY_DIR/ParallelTasks
 make
 cd $MY_DIR
-root -b Test.C
+root -b Test.C\($MY_PARALLEL\)
